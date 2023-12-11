678e00119bf6db3f59ddedfad83458b3121235a5
Jose Luis Perez Olguin <jperez@saiko.mx>
2023-12-11 16:11:13 -0600

    video de inicio
    video renderizado puesto en el inicio y fondo plano para otras páginas

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,12 @@
-import AmplusSlide1 from "./molecules/AmplusSlide1";
-import DisplayHide from "./molecules/DisplayHide";
-import GrupoAmplusSdeRL from "./molecules/GrupoAmplusSdeRL";
-import HomeAnimation from "./molecules/Home";
-import SlideText from "./molecules/SlideText";
 import ui from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={ui.slide1}>
-      <HomeAnimation
-        child1={
-          <section className={ui.inicio}>
-            <SlideText
-              animationSpeed={2}
-              words={["AMPLUS", "TECNOLOGÍA", "INSPIRACIÓN"]}
-            />
-          </section>
-        }
-        child2={
-          <div className={ui.slideA}>
-            <AmplusSlide1 />
-          </div>
-        }
-        child3={
-          <div className={ui.slideB}>
-            <GrupoAmplusSdeRL />
-          </div>
-        }
-      />
-    </div>
+    <>
+      <video autoPlay loop muted>
+        <source src="/Inicio 3.1.mp4" type="video/mp4" />
+      </video>
+      <div className={ui.slide1}></div>
+    </>
   );
 }
