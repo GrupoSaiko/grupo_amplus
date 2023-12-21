b52eef9c64e2f3c347b2ae3b21649e9ce3bb38b7
Jose Luis Perez Olguin <jperez@saiko.mx>
2023-12-21 17:34:50 -0600

    nextconfig

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
-  output: "export",
-  async headers() {
-    return [
-      {
-        source: "*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "*",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-        ],
-      },
-    ];
-  },
+  output: "export"
 };
 
 module.exports = nextConfig;
