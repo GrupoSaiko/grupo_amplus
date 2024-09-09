f7c1fbea4f947ef062478e7980db9fb52fb9079e
Jose Luis Perez Olguin <jperez@saiko.mx>
2024-09-09 15:24:52 -0600

    optimizacion y ui
    mejor rendimiento de imagenes y estilos del login

--- src/app/Menu/OptionsNavigation/index.js
+++ src/app/Menu/OptionsNavigation/index.js
@@ -1,27 +1,32 @@
-"use client"
+"use client";
 import React, { useContext } from "react";
 import Link from "next/link";
 import { ContextMenu } from "..";
-import OAuthLogin from "@/app/molecules/OAuthLogin";
-import ProfileOverview from "@/app/structure/ProfileOverview";
 
 export default function OptionsNavigation() {
-
-const hook = useContext(ContextMenu);
+  const hook = useContext(ContextMenu);
 
   return (
     <>
-      {/* <ProfileOverview/> */}
+      <Link onClick={hook.closeMenu} href="/">
+        Inicio
+      </Link>
 
-      <Link onClick={hook.closeMenu} href="/">Inicio</Link>
+      <Link onClick={hook.closeMenu} href="/#nosotros">
+        Nosotros
+      </Link>
 
-      <Link onClick={hook.closeMenu} href="/#nosotros">Nosotros</Link>
+      <Link onClick={hook.closeMenu} href={"/#servicios"}>
+        Servicios
+      </Link>
 
-      <Link onClick={hook.closeMenu} href={"/#servicios"}>Servicios</Link>
+      <Link onClick={hook.closeMenu} href={"/#productos"}>
+        Productos
+      </Link>
 
-      <Link onClick={hook.closeMenu} href={"/#productos"}>Productos</Link>
-
-      <Link onClick={hook.closeMenu} href="/#contacto">Contacto</Link>
+      <Link onClick={hook.closeMenu} href="/#contacto">
+        Contacto
+      </Link>
     </>
   );
 }
